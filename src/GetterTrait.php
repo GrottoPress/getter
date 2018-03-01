c7bc8e198a380062fd2018885cee5d5b74a67db7
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-01 10:59:01 +0000

    Remove redundant doc blocks

--- src/GetterTrait.php
+++ src/GetterTrait.php
@@ -1,40 +1,14 @@
 <?php
-
-/**
- * Getter trait
- *
- * Get private attributes of an object directly,
- * without calling getter methods.
- *
- * @package GrottoPress\Getter
- * @since 0.3.0
- *
- * @author GrottoPress (https://www.grottopress.com)
- * @author N Atta Kusi Adusei (https://twitter.com/akadusei)
- */
-
 declare (strict_types = 1);
 
 namespace GrottoPress\Getter;
 
 use Exception;
 
-/**
- * Getter trait
- *
- * @since 0.3.0
- */
 trait GetterTrait
 {
     /**
-     * Get attribute
-     *
-     * @var string $attribute Name of attribute to get
-     *
-     * @since 0.3.0
-     * @access public
-     *
-     * @return mixed Attribute
+     * @return mixed
      */
     final public function __get(string $attribute)
     {
